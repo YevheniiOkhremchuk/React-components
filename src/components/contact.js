5b89107ef8ea5cfe2c9d3a3c790b92a031cf4f84
Yevhenii <okhremchukjevhenii@.gmail.com>
2020-09-10 15:46:58 +0300

    HW-16

---
+++ src/components/contact.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './style.css';
+
+const Contact = ({ firstName, lastName, phone, gender }) => {
+    return (
+        <div className='Contact'>
+            <div className='first-name'>
+                <div className='name'>{firstName} {lastName}</div>
+                <div className='gender'>{gender}</div>
+            </div>
+            <div className='phone'>{phone}</div>
+        </div>
+    )
+}
+
+export default Contact;
