b41293741dd72b73fba2fa68859db630d334d40f
Yevhenii <okhremchukjevhenii@.gmail.com>
2020-09-10 15:55:17 +0300

    Updates

---
+++ precache-manifest.4a006a02b1973257e74ad24377cb3339.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8de26abbb97186992f2d29c0f493eb0",
+    "url": "/React-components/index.html"
+  },
+  {
+    "revision": "2bab9bb69ab24ec0ca8b",
+    "url": "/React-components/static/css/main.e6a1ac88.chunk.css"
+  },
+  {
+    "revision": "0d91e8047666164b9a84",
+    "url": "/React-components/static/js/2.915e6223.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-components/static/js/2.915e6223.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bab9bb69ab24ec0ca8b",
+    "url": "/React-components/static/js/main.161719c6.chunk.js"
+  },
+  {
+    "revision": "f59a0f0582b5ccc80faa",
+    "url": "/React-components/static/js/runtime-main.cdb2ed69.js"
+  },
+  {
+    "revision": "ab7f36c566c2b80cb351ae3a550527a8",
+    "url": "/React-components/static/media/anonym.ab7f36c5.png"
+  },
+  {
+    "revision": "6d61aca982b1be00bc40091c7e311083",
+    "url": "/React-components/static/media/man.6d61aca9.png"
+  },
+  {
+    "revision": "a8023cdfb423171bdbb54a6ae6157a15",
+    "url": "/React-components/static/media/woman.a8023cdf.png"
+  }
+]);
